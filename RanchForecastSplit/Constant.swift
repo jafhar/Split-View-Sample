cadc3a0c9ac86c4b02335db9dda54a0cbad462b0
jafharsharief <jafharsharief@gmail.com>
2017-06-16 15:11:53 +0530

    Adding constants

---
+++ RanchForecastSplit/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  RanchForecastSplit
+//
+//  Created by jafharsharief.b on 16/06/17.
+//  Copyright © 2017 Exilant. All rights reserved.
+//
+
+struct Constant {
+    static let defaultURL = "https://www.apple.com"
+    static let defaultDateFormat = "yyyy-MM-dd"
+    static let courcesKey = "courses"
+    static let titleKey = "title"
+    static let urlKey = "url"
+    static let upcomingKey = "upcoming"
+    static let startDateKey = "start_date"
+    static let defaultTitle = "Apple Co."
+    static let domainName = "ScheduleFetcher"
+}
